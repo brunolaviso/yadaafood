6d3702747d4f07f24cfe13b4a1b5543a3b516986
Bruno Laviso <brunolaviso@gmail.com>
2023-08-02 21:39:23 -0300

    feat: faz comunicacao com api airtable

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import { CardProducts } from "../../components/CardProducts";
 import { Header } from "../../components/Header";
 
 import { ContainerProducts } from './styles'
 
 export function Products() {
+  useEffect(() => {
+    fetch("https://api.airtable.com/v0/app5Vj8ekDqkUVUkD/products", {
+      headers: {
+        "Authorization": `Bearer  ${import.meta.env.VITE_API_TOKEN}`
+      }})
+      .then(response => response.json())
+      .then(response => console.log(response))
+  }, [])
+
   return (
     <>
       <Header />
